1af5caf2d9002a5cdeb2a40014216b038f13ed4d
william-zhang-fullstack <william.zhang.fullstack@gmail.com>
2021-01-24 10:35:32 -0500

    added import app test

--- sales-report/tests/test_webapp.py
+++ sales-report/tests/test_webapp.py
@@ -6,5 +6,8 @@ webapp_path = os.path.join(os.path.dirname(__file__), '..')
 sys.path.append(webapp_path)
 
 
+def test_app_exists():
+    from webapp import app
+
 def test_dummy():
     assert 1 + 1 == 2
